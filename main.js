af0af48932e5854aaf173c38d2656ab3952e98c0
joelcoder2011 <80383585+joelcoder2011@users.noreply.github.com>
2022-01-10 12:53:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,5 +22,13 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
+    Webcam.attach(camera);
 
 }
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'jpeg',
+    jpeg_quality:90
+});
