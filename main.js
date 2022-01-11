771688494150afe1f63ade5c83563214ca5d7604
joelcoder2011 <80383585+joelcoder2011@users.noreply.github.com>
2022-01-11 12:45:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,18 +11,28 @@ recognition.onresult = function(event){
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
     console.log(content);
-    speak();
+    if(content == "cheese"){
+        console.log("taking selfie");
+        speak();
+    }
+    
 }                        
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "taking selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+   setTimeout(function(){
+       take_snapshot();
+       save();
+   }, 5000);
+
 
 }
 camera = document.getElementById("camera");
@@ -32,3 +42,16 @@ Webcam.set({
     image_format:'jpeg',
     jpeg_quality:90
 });
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
